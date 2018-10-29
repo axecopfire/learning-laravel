981bcb0567279a62c9f36b9ee88e178c3bef34cc
axecopfire <sbraun990@gmail.com>
2018-10-28 22:16:29 -0400

    Migration issues, email too long in boilerplate migration

--- database-lara/database/migrations/2014_10_12_000000_create_users_table.php
+++ database-lara/database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,31 +4,26 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateUserTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+
     public function up()
     {
+        Schema::dropIfExists('users');
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('role_id')->unsigned();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('email',50)->unique();
+            $table->string('username');
             $table->string('password');
+            $table->boolean('active');
             $table->rememberToken();
             $table->timestamps();
+
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('users');
